904ee604f142c6e3f0aaa93ac10025f3464288f2
funchooooza <uroddaunich@gmail.com>
2025-07-29 09:08:19 +1000

    upd: app

--- src/main.py
+++ src/main.py
@@ -1,3 +1,7 @@
 from fastapi import FastAPI
 
-app = FastAPI(title="DNS-task")
+from api.lifespan import lifespan
+from api.routers.manage import router as manage_router
+
+app = FastAPI(title="DNS-task", lifespan=lifespan)
+app.include_router(manage_router)

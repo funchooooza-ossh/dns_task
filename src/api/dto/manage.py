98835d80ac1a3b9801bb4782005fdd3914249c17
funchooooza <uroddaunich@gmail.com>
2025-07-29 09:26:09 +1000

    add: schema meta dto

--- src/api/dto/manage.py
+++ src/api/dto/manage.py
@@ -3,3 +3,17 @@ from pydantic import BaseModel
 
 class Schemas(BaseModel):
 	schemas: list[str]
+
+
+class Field(BaseModel):
+	name: str
+	type: str
+
+
+class Table(BaseModel):
+	name: str
+	fields: list[Field]
+
+
+class Tables(BaseModel):
+	tables: list[Table]
